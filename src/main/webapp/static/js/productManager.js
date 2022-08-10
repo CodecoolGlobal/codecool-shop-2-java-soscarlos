a4d5b1d5a905c0fb702003c482789a6f54636a19
R0-M4N <fannroman850@gmail.com>
2022-08-10 13:36:57 +0200

    get data in JS properly

--- src/main/webapp/static/js/productManager.js
+++ src/main/webapp/static/js/productManager.js
@@ -1,17 +1,35 @@
 import {dataHandler} from "./dataManager.js";
 
-let categoryId;
-
 export let productManager = {
 
     addEventListeners() {
         let categorySelectId = document.querySelector("#category-select");
         categorySelectId.addEventListener("change", function() {
-            console.log(this.value);
+            let options = document.querySelectorAll("option");
+            options.forEach(option => {
+                if (option.text === this.value) {
+                    let id = option.id;
+                    let data = productManager.getProducts(id);
+                    data.then(product => console.log(product)); //TODO: Pass to HTMLFactory
+                }
+            })
+        });
+
+        let supplierSelectId = document.querySelector("#supplier-select");
+        supplierSelectId.addEventListener("change", function() {
+            let options = document.querySelectorAll("option");
+            options.forEach(option => {
+                if (option.text === this.value) {
+                    let id = option.id;
+                    let data = productManager.getProducts(id);
+                    data.then(product => console.log(product)); //TODO: Pass to HTMLFactory
+                }
+            })
         })
     },
 
     async getProducts(id) {
         let data = await dataHandler.getProducts(1);
+        return data;
     }
 }
