4bb9deab8089591cb687048c84fd3298dec74b7e
mscobblepot <tomecsko.reka@gmail.com>
2022-08-10 16:25:24 +0200

    Mocks thymeleaf in cart

--- src/main/java/com/codecool/shop/controller/api/CartServlet.java
+++ src/main/java/com/codecool/shop/controller/api/CartServlet.java
@@ -1,6 +1,8 @@
 package com.codecool.shop.controller.api;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.implementation.CartDao;
+import com.codecool.shop.model.dto.ProductDTO;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -9,13 +11,19 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(urlPatterns = {"/cart"})
 public class CartServlet extends javax.servlet.http.HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        CartDao cartDao = CartDao.getInstance();
+        List<ProductDTO> productsInCart = cartDao.getProductsDTO();
+
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
+        context.setVariable("products", productsInCart);
+
         engine.process("product/cart.html", context, resp.getWriter());
     }
 }
