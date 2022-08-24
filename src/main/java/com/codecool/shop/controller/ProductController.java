6cac8ff54350ff7c2c859c1123830929a8c9b625
carlos <soscarlos@gmail.com>
2022-08-24 09:33:57 +0200

    all jdbc classes implemented not yet tested

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -12,6 +12,7 @@ import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.dao.jdbc.ProductCategoryDaoJdbc;
 import com.codecool.shop.dao.jdbc.ShopDataManager;
 import com.codecool.shop.model.product.ProductCategory;
+import org.postgresql.ds.PGSimpleDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.thymeleaf.TemplateEngine;
@@ -22,7 +23,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
+import java.util.SortedMap;
 
 @WebServlet(urlPatterns = {"/"})
 public class ProductController extends HttpServlet {
@@ -36,13 +41,34 @@ public class ProductController extends HttpServlet {
         SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
         CartDao cartDao = CartDao.getInstance();
 
+        try {
+            FileInputStream fileInputStream = new FileInputStream("src/main/resources/connection.properties");
+            Properties properties = new Properties();
+            properties.load(fileInputStream);
+
+            String dao = (String) properties.get("dao");
+            String memory = "memory";
+            String jdbc = "jdbc";
+
+            if (dao.equals(memory)){
+                System.out.println("Load from memory");
+            } else if (dao.equals(jdbc)) {
+                System.out.println("Load from db");
+            }
+
+        } catch (FileNotFoundException e){
+            logger.error("Could not find file", e);
+            throw new RuntimeException(e);
+        } catch (IOException e){
+            logger.error("Could not read from file stream", e);
+            throw new RuntimeException(e);
+        }
+
         ShopDataManager dbManager = ShopDataManager.getInstance();
 
         DataSource dataSource = dbManager.connect();
 
         ProductCategoryDaoJdbc categoryDaoJdbc = ProductCategoryDaoJdbc.getInstance(dataSource);
-
-
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
--- src/main/java/com/codecool/shop/dao/jdbc/OrderDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/jdbc/OrderDaoJdbc.java
@@ -1,20 +1,23 @@
 package com.codecool.shop.dao.jdbc;
 
+import com.codecool.shop.model.dto.ProductDTO;
 import com.codecool.shop.model.product.Product;
+import com.codecool.shop.service.product.ProductDTOService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class OrderDaoJdbc {
 
     private static final Logger logger = LoggerFactory.getLogger(ProductDaoJdbc.class);
-
-    private final DataSource dataSource;
     private static OrderDaoJdbc instance = null;
+    private final ProductDTOService productDTOService = new ProductDTOService();
+    private final DataSource dataSource;
 
     private OrderDaoJdbc(DataSource dataSource) {
         this.dataSource = dataSource;
@@ -27,27 +30,27 @@ public class OrderDaoJdbc {
         return instance;
     }
 
-    public void addToCart(Product product) {
+    public void addToCart(ProductDTO product) {
         try (Connection conn = dataSource.getConnection()) {
             String sql = "INSERT INTO orders (product_id) VALUES (?)";
             PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            statement.setInt(1, product.getId());
+            statement.setInt(1, Integer.parseInt(product.getId()));
 
             statement.executeUpdate();
             ResultSet resultSet = statement.getGeneratedKeys();
             resultSet.next();
-            product.setId(resultSet.getInt(1));
+
         } catch (SQLException e) {
             logger.error("Could not connect with database", e);
             throw new RuntimeException(e);
         }
     }
 
-    public void removeFromCart(Product product) {
+    public void removeFromCart(ProductDTO product) {
         try (Connection connection = dataSource.getConnection()) {
             String sql = "DELETE FROM orders WHERE product_id = ?;";
             PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setInt(1, product.getId());
+            statement.setInt(1, Integer.parseInt(product.getId()));
             statement.executeUpdate();
         } catch (SQLException e) {
             logger.error("Could not connect with database", e);
@@ -56,13 +59,54 @@ public class OrderDaoJdbc {
     }
 
 
-    public Optional<Product> getProductById(String id) {
-        return Optional.empty();
+    public Optional<ProductDTO> getProductById(String id) {
+        try (Connection connection = dataSource.getConnection()) {
+            String sql = "SELECT * FROM orders WHERE product_id = ?;";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, Integer.parseInt(id));
+            ResultSet result = statement.executeQuery();
+
+            if (!result.next()) return Optional.empty();
+
+            return getProductDTO(id, result);
+
+        } catch (SQLException e) {
+            logger.error("Could not connect with database", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Optional<ProductDTO> getProductDTO(String id, ResultSet result) throws SQLException {
+        int productId = result.getInt("product_id");
+
+        ProductDaoJdbc productDao = ProductDaoJdbc.getInstance(dataSource);
+        Product product = productDao.find(productId);
+        product.setId(Integer.parseInt(id));
+
+        ProductDTO productDTO = productDTOService.getProduct(product);
+
+        return Optional.ofNullable(productDTO);
     }
 
 
-    public List<Product> getProducts() {
+    public List<ProductDTO> getProducts() {
+        try (Connection connection = dataSource.getConnection()) {
+            String sql = "SELECT * FROM orders;";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet result = statement.executeQuery();
 
-        return null;
+            List<ProductDTO> products = new ArrayList<>();
+
+            while (result.next()) {
+                int productId = result.getInt(2);
+                Optional<ProductDTO> productDTO = getProductDTO(String.valueOf(productId), result);
+                productDTO.ifPresent(products::add);
+            }
+            return products;
+
+        } catch (SQLException e) {
+            logger.error("Could not connect with database", e);
+            throw new RuntimeException(e);
+        }
     }
 }
